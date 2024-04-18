c5d03eabd80f5c8d5ede96f7437a2e7a64512a5f
mehdifadaei-irn <m_mehdi_000@yahoo.com>
2024-04-19 01:04:05 +0330

    initialize server

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { getPayLoadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  const payload = await getPayLoadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
+
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    // payload.logger.info("Next started..!!");
+
+    app.listen(PORT, async () => {
+      //   payload.logger.info(
+      //     `next,js app URL :${process.env.NEXT_PUBLIC_SERVER_URL}`
+      //   );
+    });
+  });
+};
+
+start();
